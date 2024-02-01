fd2d4c814ef64b392b348a28862c18e5c1ebff1f
KitidetSriboon <kitidet1@gmail.com>
2024-02-01 10:29:43 +0700

    ktd update 1-2-2024 10.30

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -4,12 +4,12 @@ export class GlobalConstants {
     public static appname: string = "brdFarm";
     public static yearCr: string = "2324";
     public static yearTh: string = "6667";
-    public static appversion: string = "1.0.0 Beta";
-    public static lastupdate: string = "25 ธ.ค.66"
+    public static appversion: string = "1.1.0 Beta";
+    public static lastupdate: string = "23 ม.ค.67"
     public static versionDesc = [
-        { "subject": "1.หน้าแรก สรุปข้อมูลภาพรวมเรื่องต่างๆ" },
-        { "subject": "2.เมนู แผนที่แปลงอ้อย" },
-        { "subject": "3.เมนู ฤดูหีบอ้อย" },
+        { "subject": "1.บันทึกกิจกรรมแปลง" },
+        // { "subject": "2.เมนู แผนที่แปลงอ้อย" },
+        // { "subject": "3.เมนู ฤดูหีบอ้อย" },
     ];
     public static yeardata: any = [];
     public static fmdata: any = [];
